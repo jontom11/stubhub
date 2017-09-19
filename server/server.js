e3160ab75274545fe6976ee6254430f1a9197092
Jonathan Tom <Jonathan.Y.Tom1@gmail.com>
2017-09-19 00:39:37 -0700

    adjusted server.js file with wildcard, port start, and filepath for catchall

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+const port = process.env.PORT || 3001;
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -28,29 +29,13 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/api', api);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  // The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  console.log('app.get wild card activated!')
+  res.sendFile(path.join(__dirname, '../build', '../index.html'));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // The "catchall" handler: for any request that doesn't
-// // match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
-// });
-
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+  app.listen(port);
+  console.log(`Password generator listening on ${port}`);
 module.exports = app;
